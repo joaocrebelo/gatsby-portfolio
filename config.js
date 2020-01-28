03762d8e1da47058e92f59cf5586a19c2610192c
Joao Rebelo <A74636@alunos.uminho.pt>
2020-01-28 15:16:56 +0000

    Setup

---
+++ config.js
@@ -0,0 +1,62 @@
+/* eslint-disable linebreak-style */
+'use strict';
+
+module.exports = {
+  url: 'https://rebelo.xyz',
+  pathPrefix: '/',
+  title: 'joaocrebelo',
+  subtitle: 'The personal blog of João Rebelo.',
+  copyright: '🍋 At least some rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: '',
+  useKatex: false,
+  menu: [
+    {
+      label: 'All Content',
+      path: '/'
+    },
+    {
+      label: 'Projects',
+      path: '/category/projects/'
+    },
+    {
+      label: 'Publications',
+      path: '/pages/publications'
+    },
+    {
+      label: 'Achievements',
+      path: '/pages/achievements'
+    },
+    {
+      label: 'About',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contacts',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'João Rebelo',
+    photo: '',
+    bio: 'Information Systems Engineer. Seeking new challenges. Site under construction',
+    contacts: {
+      email: 'joaorebelo@live.com',
+      facebook: '',
+      telegram: '',
+      twitter: '',
+      github: 'joaocrebelo',
+      rss: '',
+      vkontakte: '',
+      linkedin: 'joaocrebelo',
+      instagram: '',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+    }
+  }
+};
